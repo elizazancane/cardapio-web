708a4e4412cb56fa490437fc6db3551097f73269
Usuário <elizzazancane@gmail.com>
2024-06-13 10:41:20 -0300

    Alteração no Produto.cs e Categoria.cs.

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -4,9 +4,13 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace CardapioWeb.Models
 {
     [Table("Categorias")]
+    //Tabela "Categorias" do BD que vai definir toda vez
+    //que for usar as informações presentes na classe
+    //"Categoria" abaixo. 
     public class Categoria
     {
-        [Key]
+        [Key] 
+        //Key: Chave Primaria, Id
         public int Id { get; set; }
         public string Nome { get; set; }
         public string Descricao { get; set; }
--- Models/Produto.cs
+++ Models/Produto.cs
@@ -1,6 +1,30 @@
-﻿namespace CardapioWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CardapioWeb.Models
 {
+    [Table("Produtos")]
+    //A Classe "Produto" vai se relacionar com a Tabela "Produtos"
     public class Produto
     {
+        [Key]
+        public int Id { get; set;}
+
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        [Display(Name ="Nome do Produto")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        public string Descricao_curta { get;}
+
+        public string Descricao_Detalhada { get; set; }
+
+        public decimal Preco { get; set; }
+
+        public bool Produto_Preferido { get; set; }
+
+        public bool Em_Estoque { get; set; }
+
+        public virtual Categoria Categoria { get; set;}
     }
 }
