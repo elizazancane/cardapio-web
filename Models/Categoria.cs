b492bc3f1cfa81b48da7dc43444100f1f899778d
elizazancane <elizzazancane@gmail.com>
2024-06-20 10:45:33 -0300

    Alteração no AppDbContext.

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -12,7 +12,15 @@ namespace CardapioWeb.Models
         [Key] 
         //Key: Chave Primaria, Id
         public int Id { get; set; }
+
+        [Display(Name = "Nome da categoria")]
+        [Required(ErrorMessage = "Campo Obrigatório")]
         public string Nome { get; set; }
+
+        [Display(Name = "Descrição da categoria")]
+        [Required(ErrorMessage = "Campo Obrigatório")]
         public string Descricao { get; set; }
+
+        public List<Produto> Produtos { get; set; }
     }
 }
